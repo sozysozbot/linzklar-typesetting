fb67df2e809ad2672dbc7cdd5e5cd76adc6a32f8
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2021-09-04 04:47:54 +0900

    Split the resulting "complete" to create glyph files

--- split.js
+++ split.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const parse_svg = require("svg-path-parser");
 (() => {
-    const file_name = process.argv[2] ?? "incomplete-big.svg";
-    const out_path = process.argv[3] ?? "incomplete-big-res.svg";
+    const excelColumnName = require('excel-column-name');
+    const svgpath = require('svgpath');
+    const file_name = process.argv[2] ?? "complete.svg";
+    const out_path = process.argv[3] ?? "glyphs";
     const fs = require('fs');
     const text = fs.readFileSync(file_name, 'utf-8');
     const jsdom = require("jsdom");
@@ -14,48 +15,45 @@ const parse_svg = require("svg-path-parser");
     const y_width = 6.75383;
     const x_delta = 0.28;
     const y_delta = 0.26;
-    glyphs.forEach(path => {
-        if (!path.getAttribute) {
-            return [];
+    const transcription_count = new Map();
+    for (const glyph of glyphs) {
+        if (!glyph.getAttribute) {
+            continue;
         }
-        parse_svg.parseSVG(path.getAttribute("d")).flatMap(c => {
-            if (c.code === 'M') {
-                const x_index = Math.floor((c.x - x_delta) / x_width);
-                const y_index = Math.floor((c.y - y_delta) / y_width);
-                const box_id = `？${[
-                    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-                    "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ",
-                ][x_index]}${y_index}`;
-                const box = dom.window.document.getElementById(box_id) ?? (() => {
-                    const new_g = dom.window.document.createElementNS("http://www.w3.org/2000/svg", "g");
-                    const randomColor = "#" + ("000000" + (Math.random() * 0xFFFFFF | 0).toString(16)).slice(-6);
-                    new_g.setAttributeNS(null, "id", box_id);
-                    new_g.setAttributeNS(null, "stroke", randomColor);
-                    dom.window.document.getElementById("glyphs").appendChild(new_g);
-                    return dom.window.document.getElementById(box_id);
-                })();
-                box.appendChild(path);
+        const glyph_id = glyph.getAttribute("id");
+        const size = glyph_id.includes(":") ? 2 : 1;
+        const [_, glyph_transcription, column_string, row_string] = glyph_id.match(/^(.)([A-Za-z]+)([0-9]+)/);
+        const count = (transcription_count.get(glyph_transcription) ?? 0) + 1;
+        transcription_count.set(glyph_transcription, count);
+        const x_index = excelColumnName.excelColToInt(column_string) - 1;
+        const y_index = Number(row_string);
+        const old_paths = [...glyph.childNodes];
+        const new_paths = old_paths.flatMap(path => {
+            if (!path.getAttribute) {
+                return [];
+            }
+            if (size === 1) {
+                const old_path = path.getAttributeNS(null, "d");
+                const new_path = svgpath(old_path)
+                    .translate(-(x_index * x_width + x_delta), -(y_index * y_width + y_delta))
+                    .toString();
+                return [new_path];
+            }
+            else {
+                const new_path = svgpath(path.getAttributeNS(null, "d"))
+                    .translate(-(x_index * x_width + x_delta), -(y_index * y_width + y_delta))
+                    .scale(0.5)
+                    .toString();
+                return [new_path];
             }
         });
-    });
-    fs.writeFileSync(out_path, `
-<?xml version="1.0" encoding="UTF-8"?>
-<svg width="350mm" height="246.4mm" version="1.1" viewBox="0 0 250 176" xmlns="http://www.w3.org/2000/svg">
-    <g fill="none" stroke="#000" stroke-width=".365" id="glyphs">
-    ${dom.window.document.getElementById("glyphs").innerHTML}
+        fs.writeFileSync(`${out_path}/${glyph_transcription}_${size === 2 ? "large" : "small"}_${transcription_count.get(glyph_transcription)}.svg`, `<?xml version="1.0" encoding="UTF-8"?>
+<svg width="20mm" height="20mm" version="1.1" viewBox="0 0 6.75383 6.75383" xmlns="http://www.w3.org/2000/svg">
+    <!-- From ${glyph_id} -->
+    <path fill="#faa" d="m0 0 h20v20h-20" />
+    <g fill="none" stroke="#000" stroke-width=".365" >
+        ${new_paths.map(p => `<path d="${p}" />`).join("\n\t\t")}
     </g>
-</svg>
-`);
-    /*
-    fs.writeFileSync(`${out_path}/content.txt`, text_content);
-    glyph_map.forEach((value, key) => {
-        fs.writeFileSync(`${out_path}/${key}.svg`, `<?xml version="1.0" encoding="UTF-8"?>
-    <svg width="136mm" height="120mm" version="1.1" viewBox="0 0 136 120" xmlns="http://www.w3.org/2000/svg">
-        <path fill="#faa" d="m0 0 h136v120h-136" />
-        <path fill="#aff" d="m10 10 h116 v 100 h-116 z" />
-        <g stroke="#000" stroke-width="10" fill="none">
-            <g id="glyph">${value}</g>
-        </g>
-    </svg>`);
-    });*/
+</svg>`);
+    }
 })();
